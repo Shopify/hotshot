d1879699fc4be5dc66fc2519287a16b2e99cba84
Koen Vendrik <k.vendrik@gmail.com>
2016-09-27 14:06:22 -0400

    Fixes #1 and moves some logic to seperate method

--- dist/Hotshot.js
+++ dist/Hotshot.js
@@ -24,11 +24,12 @@ var Hotshot = function () {
     value: function _resetWaitInputTimer(callback) {
       var _this = this;
 
+      var waitTime = arguments.length <= 1 || arguments[1] === undefined ? this._waitForInputTime : arguments[1];
+
       //wait for user input for x amount of time
       //if there is no user input
       //reset the pressed keys register and trigger
       //the optional callback
-      var waitTime = this._waitForInputTime;
 
       clearTimeout(this._waitInputTimer);
       this._waitInputTimer = setTimeout(function () {
@@ -39,13 +40,13 @@ var Hotshot = function () {
       }, waitTime);
     }
   }, {
-    key: '_handleKey',
-    value: function _handleKey(e) {
+    key: '_checkBindingsForPressedKeys',
+    value: function _checkBindingsForPressedKeys() {
       var bindings = this._bindings;
-      var keyCode = e.keyCode;
+      var pressedKeys = this._pressedKeys;
 
-      //register pressed key
-      this._pressedKeys += '' + keyCode;
+      var match = null;
+      var shouldWait = false;
 
       //loop all key bindings and
       //check if the register matches one of the codes
@@ -60,17 +61,19 @@ var Hotshot = function () {
           var callback = _step$value.callback;
 
           var codeStr = keyCodes.join('');
-          if (this._pressedKeys === codeStr) {
+          if (pressedKeys === codeStr) {
             //pressed keys match config code
             //wait for next input
             //if there is no next input, trigger callback
-            this._resetWaitInputTimer(callback);
-            return;
+            match = { keyCodes: keyCodes, callback: callback };
+          } else if (codeStr.indexOf(pressedKeys) === 0) {
+            //if there is a shortcut
+            //registered with more chars that starts with this
+            //(e.g. user pressed gs but there is also gsp)
+            //then give user time to press the next key
+            shouldWait = true;
           }
         }
-
-        //reset timer so the pressed keys are
-        //reset if there is no more user input
       } catch (err) {
         _didIteratorError = true;
         _iteratorError = err;
@@ -86,7 +89,40 @@ var Hotshot = function () {
         }
       }
 
-      this._resetWaitInputTimer();
+      return {
+        match: match,
+        shouldWait: shouldWait
+      };
+    }
+  }, {
+    key: '_handleKey',
+    value: function _handleKey(_ref2) {
+      var keyCode = _ref2.keyCode;
+
+      //register pressed key
+      this._pressedKeys += keyCode;
+
+      //check pressed keys against config
+
+      var _checkBindingsForPres = this._checkBindingsForPressedKeys();
+
+      var match = _checkBindingsForPres.match;
+      var shouldWait = _checkBindingsForPres.shouldWait;
+
+
+      if (match) {
+        //keys match found
+        if (shouldWait) {
+          this._resetWaitInputTimer(match.callback);
+        } else {
+          this._resetWaitInputTimer(match.callback, 0);
+        }
+      } else {
+        //if no match was found yet
+        //reset timer so the pressed keys are
+        //reset if there is no more user input
+        this._resetWaitInputTimer();
+      }
     }
   }]);
 
--- src/Hotshot.js
+++ src/Hotshot.js
@@ -8,12 +8,11 @@ class Hotshot {
     document.addEventListener('keyup', this._handleKey.bind(this), false);
   }
   
-  _resetWaitInputTimer(callback){
+  _resetWaitInputTimer(callback, waitTime = this._waitForInputTime){
     //wait for user input for x amount of time
     //if there is no user input
     //reset the pressed keys register and trigger
     //the optional callback
-    const waitTime = this._waitForInputTime;
 
     clearTimeout(this._waitInputTimer);
     this._waitInputTimer = setTimeout(() => {
@@ -23,30 +22,58 @@ class Hotshot {
       }
     }, waitTime);
   }
-  
-  _handleKey(e){
+
+  _checkBindingsForPressedKeys(){
     const bindings = this._bindings;
-    const keyCode = e.keyCode;
-    
-    //register pressed key
-    this._pressedKeys += `${keyCode}`;
-      
+    const pressedKeys = this._pressedKeys;
+
+    let match = null;
+    let shouldWait = false;
+
     //loop all key bindings and
     //check if the register matches one of the codes
     for (const {keyCodes, callback} of bindings) {
       const codeStr = keyCodes.join('');
-      if (this._pressedKeys === codeStr) {
+      if (pressedKeys === codeStr) {
         //pressed keys match config code
         //wait for next input
         //if there is no next input, trigger callback
-        this._resetWaitInputTimer(callback);
-        return;
+        match = { keyCodes, callback };
+      } else if (codeStr.indexOf(pressedKeys) === 0) {
+        //if there is a shortcut
+        //registered with more chars that starts with this
+        //(e.g. user pressed gs but there is also gsp)
+        //then give user time to press the next key
+        shouldWait = true;
       }
     }
+
+    return {
+      match,
+      shouldWait
+    };
+  }
+  
+  _handleKey({ keyCode }){
+    //register pressed key
+    this._pressedKeys += keyCode;
     
-    //reset timer so the pressed keys are
-    //reset if there is no more user input
-    this._resetWaitInputTimer();
+    //check pressed keys against config
+    const { match, shouldWait } = this._checkBindingsForPressedKeys();
+
+    if (match) {
+      //keys match found
+      if (shouldWait) {
+        this._resetWaitInputTimer(match.callback);
+      } else {
+        this._resetWaitInputTimer(match.callback, 0);
+      }
+    } else {
+      //if no match was found yet
+      //reset timer so the pressed keys are
+      //reset if there is no more user input
+      this._resetWaitInputTimer();
+    }
   }
 }
 
