5f7fde3d29870d357a95724c833dc08772bda56c
Koen Vendrik <k.vendrik@gmail.com>
2016-10-05 11:45:26 -0400

    Adds konami code test

--- tests/test-hotshot.js
+++ tests/test-hotshot.js
@@ -9,14 +9,23 @@ document.addEventListener('keyup', function(e){
 
 var utils = {
     testSeq: function(config, shouldWait, callback){
-        config.chars = config.chars.split('');
+        config.chars = config.chars.split(' ');
 
         var spy = sinon.spy();
+        var zeroChars = 'up up down down left right left right enter';
 
         hotshot.bindSeq(config.keyCodes, spy);
 
         config.chars.forEach(function(char, idx){
-            KeyEvent.simulate(char.charCodeAt(0), config.keyCodes[idx]);
+            var charNum;
+
+            if (zeroChars.split(' ').indexOf(char) !== -1) {
+                charNum = 0;
+            } else {
+                charNum = char.charCodeAt(0);
+            }
+
+            KeyEvent.simulate(charNum, config.keyCodes[idx]);
         });
 
         //the lib should not wait as
@@ -38,22 +47,21 @@ describe('Hotshot.bindSeq', function() {
 
     it('ab callback fires when pressing ab', function(done) {
         utils.testSeq({
-            chars: 'ab',
+            chars: 'a b',
             keyCodes: [65, 66],
         }, true, done);
     });
 
     it('abg callback fires when pressing abg', function(done) {
         utils.testSeq({
-            chars: 'abg',
+            chars: 'a b g',
             keyCodes: [65, 66, 71],
         }, false, done);
     });
 
-    it('abg callback fires when pressing abg', function(done) {
-        //https://github.com/ccampbell/mousetrap/blob/master/tests/test.mousetrap.js#L513
+    it('konami callback fires when pressing up up down down left right left right b a enter', function(done) {
         utils.testSeq({
-            chars: 'up up down down left right left right b a enter',
+            chars: 'up up down down left right left right b a',
             keyCodes: [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
         }, false, done);
     });
