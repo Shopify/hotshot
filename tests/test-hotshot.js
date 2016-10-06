0573af5f15b6f69ff4142ef8c5e8f6896e392ab1
Koen Vendrik <k.vendrik@gmail.com>
2016-10-06 14:31:28 -0400

    Adds constructor tests

--- tests/test-hotshot.js
+++ tests/test-hotshot.js
@@ -2,19 +2,25 @@ var expect = chai.expect;
 
 var hotshot;
 var WAIT_INPUT_TIME = 500;
+var constructorSpy = sinon.spy();
 
 // document.addEventListener('keyup', function(e){
 //     console.log('PRESS', e.keyCode);
 // });
 
 var utils = {
-    testBindSeq: function(config, shouldWait, callback){
-        config.chars = config.chars.split(' ');
+    testSeq: function(config, shouldWait, callback, options){
+        options = options || {};
+        spy = options.spy || sinon.spy();
 
-        var spy = sinon.spy();
         var zeroChars = 'up up down down left right left right enter';
+        var expectCalls = options.expectCalls || 1;
 
-        hotshot.bindSeq(config.keyCodes, spy);
+        if (options.dontBind !== true) {
+            hotshot.bindSeq(config.keyCodes, spy);
+        }
+
+        config.chars = config.chars.split(' ');
 
         config.chars.forEach(function(char, idx){
             var charNum;
@@ -31,7 +37,7 @@ var utils = {
         //the lib should not wait as
         //there is no shortcut with more letters
         setTimeout(function(){
-            expect(spy.callCount).to.equal(1, 'callback should fire once');
+            expect(spy.callCount).to.equal(expectCalls, 'callback should fire '+expectCalls+' times');
             callback();
         }, (shouldWait ? WAIT_INPUT_TIME : 0));
     }
@@ -39,28 +45,60 @@ var utils = {
 
 before(function() {
     hotshot = new Hotshot({
-        waitForInputTime: WAIT_INPUT_TIME
+        waitForInputTime: WAIT_INPUT_TIME,
+        seqs: [{
+            keyCodes: [65, 66, 67],
+            callback: constructorSpy
+        }, {
+            keyCodes: [49, 50, 51, 52],
+            callback: constructorSpy
+        }]
     });
+});
+
+describe('Hotshot Constructor', function(done) {
+
+    it('3 letter sequence (a b c)', function(done) {
+        utils.testSeq({
+            chars: 'a b c',
+            keyCodes: [65, 66, 67],
+        }, false, done, {
+            dontBind: true,
+            spy: constructorSpy
+        });
+    });
+
+    it('4 letter sequence (1 2 3 4)', function(done) {
+        utils.testSeq({
+            chars: '1 2 3 4',
+            keyCodes: [49, 50, 51, 52],
+        }, false, done, {
+            dontBind: true,
+            spy: constructorSpy,
+            expectCalls: 2
+        });
+    });
+
 });
 
 describe('Hotshot.bindSeq', function() {
 
     it('2 letter sequence (a b)', function(done) {
-        utils.testBindSeq({
+        utils.testSeq({
             chars: 'a b',
             keyCodes: [65, 66],
         }, true, done);
     });
 
     it('3 letter sequence that starts with already registered two letter sequence (a b g)', function(done) {
-        utils.testBindSeq({
+        utils.testSeq({
             chars: 'a b g',
             keyCodes: [65, 66, 71],
         }, false, done);
     });
 
     it('long sequence (konami code)', function(done) {
-        utils.testBindSeq({
+        utils.testSeq({
             chars: 'up up down down left right left right b a',
             keyCodes: [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
         }, false, done);
